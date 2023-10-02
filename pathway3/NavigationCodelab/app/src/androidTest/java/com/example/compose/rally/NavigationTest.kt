c4354ae7c26609f8a8cc3e8421b8cb4939a77cf6
Echung93 <kty309@gmail.com>
2023-10-02 14:39:00 +0900

    [Modify] 스크롤, 네비게이션 검증 테스트

--- pathway3/NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
+++ pathway3/NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
@@ -5,8 +5,10 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
+import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.fail
 import org.junit.Before
 import org.junit.Rule
@@ -49,4 +51,21 @@ class NavigationTest {
             .onNodeWithContentDescription("Accounts Screen")
             .assertIsDisplayed()
     }
+
+    @Test
+    fun rallyNavHost_clickAllBills_navigateToBills() {
+        composeTestRule.onNodeWithContentDescription("All Bills")
+            .performScrollTo()
+            .performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, "bills")
+    }
+
+    /**
+     * 향후
+     * https://developer.android.com/guide/navigation/testing 을
+     * 통해 더 공부 예정
+    **/
+
 }
